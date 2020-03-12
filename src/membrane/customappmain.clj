6d9506bb606bfd8c6184d0d96c9ae7ca5348e160
Adrian Smith <smith.adriane@gmail.com>
2020-03-12 15:15:28 -0700

    commenting out some code that's causing docs not to generate.

--- src/membrane/customappmain.clj
+++ src/membrane/customappmain.clj
@@ -1,11 +1,11 @@
 (ns membrane.customappmain
-  (:require membrane.uibuilder
+  (:require ;; membrane.uibuilder
             clojure.main)
   (:gen-class))
 
 
 (def main-class-loader @clojure.lang.Compiler/LOADER)
 (defn -main [& args]
-  (clojure.main/with-bindings
+  #_(clojure.main/with-bindings
     (let [project-path (java.lang.System/getProperty "project.path")]
       (membrane.uibuilder/run-project (read-string (slurp project-path))))))
