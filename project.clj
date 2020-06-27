b3d2037f4b68bf4907855728d4cc1359b2ab7d0f
Adrian Smith <smith.adriane@gmail.com>
2020-06-26 17:09:18 -0700

    changing version to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.phronemophobic/membrane "0.9.9-beta"
+(defproject com.phronemophobic/membrane "0.9.9-beta-SNAPSHOT"
   :description "A platform agnostic library for creating user interfaces"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"
