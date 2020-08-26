dc6dc9e9f017cdb306b0b88733a4438da16d7d96
Adrian Smith <smith.adriane@gmail.com>
2020-08-26 10:16:13 -0700

    updating version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.phronemophobic/membrane "0.9.12-beta-SNAPSHOT"
+(defproject com.phronemophobic/membrane "0.9.12-beta"
   :description "A platform agnostic library for creating user interfaces"
   :url "https://github.com/phronmophobic/membrane"
   :license {:name "Apache License, Version 2.0"
