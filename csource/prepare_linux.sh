666c9bd1433f07f950022037817de97d6a970915
Adrian Smith <smith.adriane@gmail.com>
2021-07-17 17:06:54 -0700

    linux workflow updates.

--- csource/prepare_linux.sh
+++ csource/prepare_linux.sh
@@ -2,7 +2,11 @@
 set -o errexit -o nounset -o pipefail
 
 apt-get update -y
-apt-get install fontconfig libfontconfig1-dev curl zip clang -y
+apt-get install fontconfig libfontconfig1-dev curl zip -y
+
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+ls /usr/bin/clang*
 
 # update-alternatives --install /usr/bin/cc cc /usr/bin/clang-3.5 100
 # update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-3.5 100
