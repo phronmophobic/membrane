bda6a1fa3c4fbc383208717f8ffb92b36c34e86a
Adrian Smith <smith.adriane@gmail.com>
2021-08-24 12:58:06 -0700

    Bumping version to 0.9.27.5-beta.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.phronemophobic/membrane "0.9.27.4-beta"
+(defproject com.phronemophobic/membrane "0.9.27.5-beta"
   :description "A platform agnostic library for creating user interfaces"
   :url "https://github.com/phronmophobic/membrane"
   :license {:name "Apache License, Version 2.0"
