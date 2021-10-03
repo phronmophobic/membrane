63cc6d6ba09924cbdde6c223395e05fe52f595a7
Adrian Smith <smith.adriane@gmail.com>
2021-10-02 23:22:21 -0700

    fix compile script.

--- csource/compile_macosx.sh
+++ csource/compile_macosx.sh
@@ -69,6 +69,6 @@ clang++ \
 #     -o testglfw \
 #     testglfw.cpp skia.cpp
 
-cp libmembraneskia.dylib ../resources/darwin
+cp libmembraneskia-${arch}.dylib ../resources/darwin/libmembraneskia.dylib
 
 echo 'done'
