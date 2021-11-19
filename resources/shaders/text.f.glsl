42dab9f48e61bdfb1112c284b1a261584d626683
Adrian Smith <smith.adriane@gmail.com>
2021-11-19 11:10:36 -0800

    Delete old, unused resources.

--- resources/shaders/text.f.glsl
+++
@@ -1,8 +0,0 @@
-varying vec2 texpos;
-uniform sampler2D tex;
-// uniform vec4 color;
-
-void main(void) {
-   gl_FragColor = vec4(1, 1, 1, texture2D(tex, texpos).a) * gl_Color;
-   // gl_FragColor = gl_Color;
-}
