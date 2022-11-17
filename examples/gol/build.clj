b22eba53f668069e1cc49747feeb8b06c800c93e
Adrian Smith <smith.adriane@gmail.com>
2022-11-17 14:28:57 -0800

    Update gol example.

--- examples/gol/build.clj
+++ examples/gol/build.clj
@@ -1,29 +1,41 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
-            [org.corfield.build :as bb]))
+  (:require [clojure.tools.build.api :as b]
+            [clojure.string :as str]))
 
-(def lib 'net.clojars.membrane/gol)
-(def version "0.1.0-SNAPSHOT")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def lib 'com.phronemophobic.membrane/gol)
+(def version "1.0")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn test "Run the tests." [opts]
-  (bb/run-tests opts))
+(defn clean [_]
+  (b/delete {:path "target"}))
 
-(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/run-tests)
-      (bb/clean)
-      (bb/jar)))
+(defn jar [opts]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
 
-(defn install "Install the JAR locally." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/install)))
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/deploy)))
+(defn deploy [opts]
+  (jar opts)
+  (try ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+        (merge {:installer :remote
+                :artifact jar-file
+                :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+               opts))
+       (catch Exception e
+         (if-not (str/includes? (ex-message e) "redeploying non-snapshots is not allowed")
+           (throw e)
+           (println "This release was already deployed."))))
+  opts)
+
+
+
