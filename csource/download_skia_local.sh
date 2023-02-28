22d9c35189b26650444a72ba5afbafff8ad6b2f9
Adrian Smith <smith.adriane@gmail.com>
2023-02-27 18:06:24 -0800

    add convenience script for local dev.

---
+++ csource/download_skia_local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
+export arch=arm64
+export platform="macosx"
+
+./download_skia.sh
+
+
+
