59a7361a64859210a47c9152e3f05a393b3089b7
Adrian Smith <smith.adriane@gmail.com>
2023-03-06 17:46:20 -0800

    Bumping version to "0.12.2-beta".

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/membrane)
-(def version "0.12.1-beta")
+(def version "0.12.2-beta")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
