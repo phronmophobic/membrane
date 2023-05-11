c343a353f6f39a2f683f05b3c8f5b471f497270a
Adrian Smith <smith.adriane@gmail.com>
2023-05-11 10:11:03 -0700

    Bump to 0.13-beta.

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]))
 
 (def lib 'com.phronemophobic/membrane)
-(def version "0.13.0-beta-SNAPSHOT")
+(def version "0.13.0-beta")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
--- csource/deploy_clojars.py
+++ csource/deploy_clojars.py
@@ -31,7 +31,7 @@ run(['cp',
 
 # cp libmembraneskia-${arch}.${shared_suffix} ${platform}-${resource_suffix}/resources/${resource_prefix}-${resource_suffix}/libmembraneskia.${shared_suffix}
 
-version="0.13-beta-SNAPSHOT"
+version="0.13-beta"
 
 print('using version: "{version}"'.format(version=version))
 
