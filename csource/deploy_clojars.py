fb5ff734d9e01e9f23b8d63edacd8778a635054e
Adrian Smith <smith.adriane@gmail.com>
2023-07-14 11:04:47 -0700

    Bump skialib to 0.14-beta-SNAPSHOT.

--- csource/deploy_clojars.py
+++ csource/deploy_clojars.py
@@ -31,7 +31,7 @@ run(['cp',
 
 # cp libmembraneskia-${arch}.${shared_suffix} ${platform}-${resource_suffix}/resources/${resource_prefix}-${resource_suffix}/libmembraneskia.${shared_suffix}
 
-version="0.13-beta"
+version="0.14-beta-SNAPSHOT"
 
 print('using version: "{version}"'.format(version=version))
 
