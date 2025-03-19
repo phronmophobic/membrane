d63a41e711c872dbfc64128886fe1a5b73b1ba2e
Adrian Smith <smith.adriane@gmail.com>
2025-03-18 23:04:15 -0700

    Try to make local builds less prone to apple quarantining problems.

--- csource/compile_local.sh
+++ csource/compile_local.sh
@@ -8,8 +8,12 @@ cd "$DIR"
 
 export arch=arm64
 
+## deleting the file seems to help workaround apple quarantining?
+rm -f libmembraneskia-${arch}.dylib
 ./compile_macosx.sh
 
+## deleting the file seems to help workaround apple quarantining?
+rm ./macos-aarch64/resources/darwin-aarch64/libmembraneskia.dylib
 cp libmembraneskia-${arch}.dylib ./macos-aarch64/resources/darwin-aarch64/libmembraneskia.dylib
 
 
