1fb84ff77ac2356a454feb66608ffd83f87c079d
Adrian Smith <smith.adriane@gmail.com>
2025-03-18 23:03:51 -0700

    Bump skia release version.
    Same skia commit though.

--- csource/download_skia.sh
+++ csource/download_skia.sh
@@ -13,7 +13,7 @@ mkdir -p libs
 pushd libs
 
 
-RELEASE="f5fefe5245098be43cb608eace5e14d67cdc09e6-1"
+RELEASE="f5fefe5245098be43cb608eace5e14d67cdc09e6-2"
 URL="https://github.com/phronmophobic/skia-build/releases/download/${RELEASE}/Skia-${RELEASE}-${platform}-Release-${arch}.zip"
 
 curl -L -o skia.zip "${URL}"
