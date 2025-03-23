7f1df40d4ed642a9c285c678f84ea53e24cd536d
Adrian Smith <smith.adriane@gmail.com>
2025-03-23 14:07:19 -0700

    fix typo in skialib linux compile script.

--- csource/compile_linux.sh
+++ csource/compile_linux.sh
@@ -6,19 +6,21 @@ set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
+skia_root="./libs/skia"
+
 c++ \
     -fPIC \
-    -I ./libs/skia \
-    -I ./libs/skia/include/gpu \
-    -I ./libs/skia/include/gpu/gl \
-    -I ./libs/skia/include/core \
-    -I ./libs/skia/include/utils \
-    -I ./libs/skia/include/private \
+    -I "$skia_root" \
+    -I "$skia_root"/include/gpu \
+    -I "$skia_root"/include/gpu/gl \
+    -I "$skia_root"/include/core \
+    -I "$skia_root"/include/utils \
+    -I "$skia_root"/include/private \
     -I "$skia_root"/include/codec \
-    -L ./libs/skia/out/Release-${arch} \
+    -L "$skia_root"/out/Release-${arch} \
     -Wl,--whole-archive \
-    ./libs/skia/out/Release-${arch}/libskia.a \
-    ./libs/skia/out/Release-${arch}/libskparagraph.a \
+    "$skia_root"/out/Release-${arch}/libskia.a \
+    "$skia_root"/out/Release-${arch}/libskparagraph.a \
     -Wl,--no-whole-archive \
     -shared \
     -std=c++17 \
